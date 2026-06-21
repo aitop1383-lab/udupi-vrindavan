ecf1f3cfc0056638928443d0a05b430aaa78ae5f
raazkumar24 <raazkumar625@gmail.com>
2026-06-21 09:12:53 +0530

    speedinsights

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { HelmetProvider } from 'react-helmet-async';
+ 
+// Vercel Speed Insights
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <App />
+      <SpeedInsights />
     </HelmetProvider>
-  </StrictMode>,
+  </StrictMode>
 );
