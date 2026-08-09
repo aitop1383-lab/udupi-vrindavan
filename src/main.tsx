f8ca0a18170f2d4e123631447042e4f000c590cb
raazkumar24 <raazkumar625@gmail.com>
2026-08-09 21:25:47 +0530

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+//its important to import the analytics after the app component so that it can track the page views correctly
 import { Analytics } from '@vercel/analytics/react';
 
 import App from './App.tsx';
